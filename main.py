99014c89f2647554770fab2063cb88e8fe3db808
Whitsonjj@gmail.com <146009318+Jwhit91657@users.noreply.github.com>
2023-10-07 15:03:37 -0700

    added boiler plate csv reader

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import csv
+
+def read_csv():
+    return
+
+def main():
+    print("starting program...")
+    data = read_csv()
+    print("program finished.")
+    
+if __name__ == '__main__':
+    main()
